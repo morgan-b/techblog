fe3fc3b673096ded554057587e4ad00d924f8262
Morgan Bailey <mabailey079@Gmail.com>
2021-04-05 15:59:16 -0400

    fixing server

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({ helpers });
 
 
 const sess = {
@@ -31,12 +32,9 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(require('./controllers/'))
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, function() {
-    console.log('Server listening on port 3001');
-    
-    });
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
